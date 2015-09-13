30aef7491e9734a97bceb46b5ad6fb13bbfe4361
Brian Lambert <keldoniaskylar@gmail.com>
2015-09-12 22:11:39 -0700

    Add user following

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,6 @@
 class MicropostsController < ApplicationController
-before_action :logged_in_user,  only: [:create, :destroy]
-before_action :correct_user,    only: :destroy
+  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -10,6 +10,7 @@ before_action :correct_user,    only: :destroy
     else
       @feed_items = []
       render 'static_pages/home'
+    end
   end
 
   def destroy
